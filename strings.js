5a395481b2849dbe44bf43d759856f6082fbade3
Rajan Yadav <rajan.yadav2024@lpu.in>
2026-07-02 05:01:55 +0530

    learn some basics of string

---
+++ strings.js
@@ -0,0 +1,10 @@
+let name1 = "rajan"
+let name2 = "yadav"
+
+console.log(name1.length);
+console.log(name1.concat(name2, "i", "am best"));
+console.log(`this is me ${44} and again this is me ${56}`);
+console.log(name2.length);
+console.log(name1.toUpperCase());
+console.log(name1.toLowerCase());
+console.log(name2.slice(1,4));
